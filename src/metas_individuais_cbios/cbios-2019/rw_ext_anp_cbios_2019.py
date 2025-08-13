8292ee90e0d518307f4ac930f145931b9ffa8773
dwbessa <danielbessa01@gmail.com>
2025-08-13 15:12:37 -0300

    fix: constants

--- src/metas_individuais_cbios/cbios-2019/rw_ext_anp_cbios_2019.py
+++ src/metas_individuais_cbios/cbios-2019/rw_ext_anp_cbios_2019.py
@@ -1,54 +1,42 @@
 import os
+import requests
+import logging
 import pandas as pd
 from io import BytesIO
-from google.cloud import bigquery, storage
+from google.cloud import bigquery
 from datetime import date
-import requests
-
-BASE_URL = (
-	"https://www.gov.br/anp/pt-br/assuntos/renovabio/metas/2019/metas-individ"
-	"uais-compulsorias-2019.xlsx"
+from constants import (
+	BASE_URL,
+	RAW_DATASET,
+	CBIOS_2019_TABLE,
+	MAPPING_COLUMNS
 )
+
+logging.basicConfig(level=logging.INFO)
+
 
 def rw_ext_anp_cbios_2019():
 	try:
+		logging.info("Iniciando o download do arquivo Excel...")
 		response = requests.get(BASE_URL, verify=False)
 		response.raise_for_status()
 		file_content = BytesIO(response.content)
-		filename = "metas-individuais-compulsorias-2019.xlsx"
-
-		bucket_name = os.getenv("GCP_BUCKET_NAME")
-		dest_path = f"anp/{filename}"
-		project_id = os.getenv("GOOGLE_CLOUD_PROJECT", "ext-ecole-biomassa")
-		client = storage.Client(project=project_id)
-		bucket = client.bucket(bucket_name)
-		blob = bucket.blob(dest_path)
-		file_content.seek(0)
-		blob.upload_from_file(file_content)
-		print(f"Arquivo enviado para: gs://{bucket_name}/{dest_path}")
-
+		logging.info("Download concluído.")
 	except Exception as e:
-		print(f"Erro ao fazer upload do arquivo: {e}")
+		logging.warning(f"Erro ao fazer download do arquivo: {e}")
 		return None
 
 	df = pd.read_excel(file_content)
+	df = df.iloc[:-2]
 
-	df.rename(columns={
-		'Código do\nAgente Regulado': 'codigo_agente_regulado',
-		'CNPJ': 'cnpj',
-		'Razão Social': 'razao_social',
-		'Somatório das Emissões \n(tCO2 equivalente)': 'somatorio_emissoes',
-		'Participação \nde Mercado (%)': 'participacao_mercado',
-		'Meta Individual 2019\n(CBIO)': 'meta_individual_2019',
-		'(8/365) * \n(Meta Individual 2019)\n(CBIO)': 'meta_individual_2019_diaria',
-	}, inplace=True)
+	df.rename(columns=MAPPING_COLUMNS, inplace=True)
 
 	df['codigo_agente_regulado'] = df['codigo_agente_regulado'].astype(str)
 
 	client = bigquery.Client()
 	project_id = os.getenv("GOOGLE_CLOUD_PROJECT", "ext-ecole-biomassa-468317")
-	bq_dataset = "rw_ext_anp"
-	table_name = "cbios_2019"
+	bq_dataset = RAW_DATASET
+	table_name = CBIOS_2019_TABLE
 
 	table_id = f"{project_id}.{bq_dataset}.{table_name}"
 
@@ -59,16 +47,16 @@ def rw_ext_anp_cbios_2019():
 	partition_key = date.today().strftime('%Y%m%d')
 
 	partitioned_table_id = f"{table_id}${partition_key}"
-	print(f"Inserting data for partition: {partition_key}")
-	print(f"Total rows to insert: {len(df)}")
+	logging.info(f"Inserting data for partition: {partition_key}")
+	logging.info(f"Total rows to insert: {len(df)}")
 
 	job = client.load_table_from_dataframe(
 		df, partitioned_table_id, job_config=job_config
 	)
 	job.result()
-	print(f"Data for {partition_key} inserted successfully.")
+	logging.info(f"Data for {partition_key} inserted successfully.")
 
-	print("Data insertion completed!")
+	logging.info("Data insertion completed!")
 
 if __name__ == "__main__":
 	rw_ext_anp_cbios_2019()
