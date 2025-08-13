de2b9757f4f0bd9a337d3535d898a28deac8e3a2
dwbessa <danielbessa01@gmail.com>
2025-08-13 15:57:26 -0300

    feat: cbios2019 terraform and dag ready

---
+++ terraform/modules/metas_individuais_cbios/main.tf
@@ -0,0 +1,12 @@
+resource "google_cloud_run_v2_job" "extracao_metas_cbios_2019_job" {
+  name     = "extracao-metas-cbios-2019-job"
+  location = var.region
+
+  template {
+    template {
+        containers {
+            image = "${var.jobs_image_base_url}/run-extracao-metas-cbios-2019-job:${var.image_version}"
+        }
+    }
+  }
+}
