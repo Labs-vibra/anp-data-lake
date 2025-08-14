1c9ba8686cd657201fdf48e623bee5eccee6bc2a
fenol64 <fenol64.dev@gmail.com>
2025-08-14 16:23:20 -0300

    terraform

--- terraform/modules/logistica/main.tf
+++ terraform/modules/logistica/main.tf
@@ -10,3 +10,16 @@ resource "google_cloud_run_v2_job" "extracao_logistica_job" {
     }
   }
 }
+
+resource "google_cloud_run_v2_job" "extracao_logistica_job" {
+  name     = "extracao-logistica-01-job"
+  location = var.region
+
+  template {
+    template {
+        containers {
+            image = "${var.jobs_image_base_url}/run-extracao-logistica-01:${var.image_version}"
+        }
+    }
+  }
+}
