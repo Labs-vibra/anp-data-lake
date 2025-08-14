6d2531f7d38d31d0bfc1ca45e9c49c3c81b1daae
fenol64 <fenol64.dev@gmail.com>
2025-08-14 18:48:09 -0300

    right types

--- db/schemas/raw/ddl_metas_individuais_compulsorias_2020.sql
+++ db/schemas/raw/ddl_metas_individuais_compulsorias_2020.sql
@@ -1,9 +1,9 @@
 # CREATE TABLE IF NOT EXISTS rw_ext_anp_cbios_2020 (
     razao_social STRING,
-    codigo_agente_regulado INT,
+    codigo_agente_regulado STRING,
     cnpj STRING,
-    meta_individual_2020 FLOAT64,
-    meta_individual_2019_diaria FLOAT64,
-    toal FLOAT64,
+    meta_individual_2020 STRING,
+    meta_individual_2019_diaria STRING,
+    toal STRING,
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 ) PARTITION BY DATE(data_criacao);
