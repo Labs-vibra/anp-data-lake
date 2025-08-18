7a55025e035215b55fe23c347cf894ac4cf298fe
fenol64 <fenol64.dev@gmail.com>
2025-08-18 14:54:17 -0300

    right schema

--- db/schemas/raw/ddl_logistics_03.sql
+++ db/schemas/raw/ddl_logistics_03.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS rw_ext_anp.logistics_03 (
-    periodo DATE,
+    periodo STRING,
     produto STRING,
     uf_origem STRING,
     uf_destino STRING,
     vendedor STRING,
     comprador STRING,
-    qtd_produto_liquido FLOAT64,
+    qtd_produto_liquido STRING,
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 ) PARTITION BY DATE(data_criacao);
