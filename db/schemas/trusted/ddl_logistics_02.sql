19df7d42c96f473f7ccbc503da841cd2222831af
luaraggio <luararaggio@hotmail.com>
2025-08-19 15:30:53 -0300

    feat(feature/51): change a file name in logistics 02 trusted schema

--- db/schemas/trusted/ddl_logistics_02.sql
+++ db/schemas/trusted/ddl_logistics_02.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS td_ext_anp.logistica_02 (
+CREATE TABLE IF NOT EXISTS td_ext_anp.logistics_02 (
     periodo DATE,
     uf_destino STRING,
     produto STRING,
     vendedor STRING,
-    quantidade_produto_liquido NUMERIC,
+    qtd_produto_liquido NUMERIC,
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 ) PARTITION BY DATE(data_criacao);
