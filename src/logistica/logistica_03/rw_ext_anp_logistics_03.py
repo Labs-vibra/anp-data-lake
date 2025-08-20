70ae26799ba0b5014ddd5e5fb812eac33ce1c4bd
dwbessa <danielbessa01@gmail.com>
2025-08-20 17:34:45 -0300

    feat: logistica 3 done

---
+++ src/logistica/logistica_03/rw_ext_anp_logistics_03.py
@@ -0,0 +1,30 @@
+from io import BytesIO
+from constants import CONGENERES_SALES_FILE_PATH, BUCKET_NAME, COLUMNS_MAPPING, LOGISTIC_03_TABLE_NAME
+from google.cloud import storage
+import pandas as pd
+
+def get_file_bytes(file_name):
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(file_name)
+    file_bytes = blob.download_as_string()
+    return BytesIO(file_bytes)
+
+def insert_data_to_bigquery(df):
+    from google.cloud import bigquery
+    client = bigquery.Client()
+    job_config = bigquery.LoadJobConfig(
+        write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    )
+    job = client.load_table_from_dataframe(df, LOGISTIC_03_TABLE_NAME, job_config=job_config)
+    job.result()
+    print(f"Loaded {job.output_rows} rows into {LOGISTIC_03_TABLE_NAME}.")
+
+def process_congeneres_sales():
+    file_bytes = get_file_bytes(CONGENERES_SALES_FILE_PATH)
+    df = pd.read_csv(file_bytes, sep=';', encoding="latin1", dtype=str)
+    df.rename(columns=COLUMNS_MAPPING, inplace=True)
+    insert_data_to_bigquery(df)
+
+if __name__ == "__main__":
+    process_congeneres_sales()
