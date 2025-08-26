8e103d9ba4f13c5da1f46edf82c0a299b8e22d06
fenol64 <fenol64.dev@gmail.com>
2025-08-26 13:41:21 -0300

    import fiz

--- airflow/dags/market-share.py
+++ airflow/dags/market-share.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.utils.task_group import TaskGroup
-from utils.operators import exec_cloud_run_job, populate_table
+from utils.operators import exec_cloud_run_job
 
 default_args = {
     'owner': 'airflow',
