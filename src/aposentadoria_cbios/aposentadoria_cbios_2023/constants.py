a2544b4471974c682cee5eba732e2fb936db70e1
Luara Raggio <luararaggio@hotmail.com>
2025-08-26 13:55:25 -0300

    feature(feat/70): adds setup for aposentadoria cbios raw/extraction

---
+++ src/aposentadoria_cbios/aposentadoria_cbios_2023/constants.py
@@ -0,0 +1,18 @@
+BASE_URL = (
+    "https://www.b3.com.br/pt_br/b3/sustentabilidade/produtos-e-servicos-esg/"
+    "credito-de-descarbonizacao-cbio/cbio-consultas/"
+)
+
+RAW_DATASET = "rw_ext_anp"
+
+APOSENTADORIA_CBIOS_2023_TABLE = "aposentadoria_cbios_2023"
+
+# MAPPING_COLUMNS = {
+# 		'Código do\nAgente Regulado': 'codigo_agente_regulado',
+# 		'CNPJ': 'cnpj',
+# 		'Razão Social': 'razao_social',
+# 		'Somatório das Emissões \n(tCO2 equivalente)': 'somatorio_emissoes',
+# 		'Participação \nde Mercado (%)': 'participacao_mercado',
+# 		'Meta Individual 2019\n(CBIO)': 'meta_individual_2019',
+# 		'(8/365) * \n(Meta Individual 2019)\n(CBIO)': 'meta_individual_2019_diaria',
+# }
