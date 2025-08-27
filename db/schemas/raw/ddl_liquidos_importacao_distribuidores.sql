cd80a56be1f2e31d98ca943f5bbb6b19a0f75f08
leeticiamm <leeticiam@hotmail.com>
2025-08-27 15:04:20 -0300

    feat: create liquidos_importacao_distribuidores raw

---
+++ db/schemas/raw/ddl_liquidos_importacao_distribuidores.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS rw_ext_anp.liquidos_importacao_distribuidores (
+    ano STRING,
+    mes STRING,
+    distribuidor STRING,
+    regiao STRING,
+    uf STRING,
+    codigo_produto STRING,
+    nome_produto STRING,
+    descricao_produto STRING,
+    regiao_origem STRING,
+    uf_origem STRING,
+    quantidade_produto_mil_m3 STRING,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) PARTITION BY DATE(data_criacao);
