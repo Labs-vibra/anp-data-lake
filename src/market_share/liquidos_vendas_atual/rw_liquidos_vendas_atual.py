c939320363707db66e929308ca6f32f2cada3904
7vanger <vitor.lgdasilva@gmail.com>
2025-08-27 17:01:42 -0300

    Mudanças no nome do arquivo como também no nome da constante

--- src/market_share/liquidos_vendas_atual/rw_liquidos_vendas_atual.py
+++ src/market_share/liquidos_vendas_atual/rw_liquidos_vendas_atual.py
@@ -5,7 +5,7 @@ from datetime import date
 from google.cloud import storage, bigquery
 from constants import (
     BUCKET_NAME,
-    liquidos_vendas_atual,
+    LIQUIDOS_VENDAS_ATUAL,
     PROJECT_ID,
     BQ_DATASET,
     TABLE_NAME
@@ -64,9 +64,9 @@ def rw_ext_anp_liquidos_vendas_atual():
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(liquidos_vendas_atual)
+    blob = bucket.blob(LIQUIDOS_VENDAS_ATUAL)
 
-    logging.info(f"Baixando arquivo {liquidos_vendas_atual} do bucket {BUCKET_NAME}...")
+    logging.info(f"Baixando arquivo {LIQUIDOS_VENDAS_ATUAL} do bucket {BUCKET_NAME}...")
     data_bytes = blob.download_as_bytes()
 
     df = pd.read_csv(BytesIO(data_bytes), sep=";", encoding="latin1")
