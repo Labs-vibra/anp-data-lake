47e90b8c7e3f299d61cbffeea87a5ed5f2f475e5
Luara Raggio <luararaggio@hotmail.com>
2025-08-27 09:30:08 -0300

    feature(feat/70): adds aposentadoria 2023 URL to constants file

--- src/aposentadoria_cbios/aposentadoria_cbios_2023/constants.py
+++ src/aposentadoria_cbios/aposentadoria_cbios_2023/constants.py
@@ -7,12 +7,9 @@ RAW_DATASET = "rw_ext_anp"
 
 APOSENTADORIA_CBIOS_2023_TABLE = "aposentadoria_cbios_2023"
 
-# MAPPING_COLUMNS = {
-# 		'Código do\nAgente Regulado': 'codigo_agente_regulado',
-# 		'CNPJ': 'cnpj',
-# 		'Razão Social': 'razao_social',
-# 		'Somatório das Emissões \n(tCO2 equivalente)': 'somatorio_emissoes',
-# 		'Participação \nde Mercado (%)': 'participacao_mercado',
-# 		'Meta Individual 2019\n(CBIO)': 'meta_individual_2019',
-# 		'(8/365) * \n(Meta Individual 2019)\n(CBIO)': 'meta_individual_2019_diaria',
-# }
+MAPPING_COLUMNS = {
+        'Data': 'data',
+        'Quantidade (Parte Obrigada)': 'quantidade_parte_obrigada',
+        'Quantidade (Parte Não Obrigada)': 'quantidade_parte_nao_obrigada',
+        'Totalização': 'totalizacao'
+}
