daf66c30b32a75c357916a9ce2ae03d67658e5b5
Luara Raggio <luararaggio@hotmail.com>
2025-08-27 21:20:02 -0300

    feat(feature/90): adds 'liquidos - historico entregas' trusted schema

---
+++ db/schemas/trusted/ddl_liquidos_historico_vendas.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS td_ext_anp.liquidos_vendas_historico (
+    data DATE,
+    distribuidor STRING,
+    codigo_produto STRING,
+    nome_produto STRING,
+    regiao_origem STRING,
+    uf_origem STRING,
+    regiao_destinatario STRING,
+    uf_destino STRING,
+    quantidade_produto_mil_m3 NUMERIC,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) PARTITION BY DATE(data_criacao);
