b4eab6ba28638d4840a6b89c436e2bf9ab147264
7vanger <vitor.lgdasilva@gmail.com>
2025-09-02 13:48:42 -0300

    mudança de nome das variaveis do arquivo

--- src/market_share/liquidos_vendas_atual/constants.py
+++ src/market_share/liquidos_vendas_atual/constants.py
@@ -3,7 +3,7 @@ import os
 BUCKET_NAME= os.getenv("BUCKET_NAME", "ext-ecole-biomassa")
 
 #RAW Liquidos de vendas atual
-LIQUIDOS_VENDA_ATUAL = "anp/market_share/extracao/20250825_LIQUIDOS_VENDAS_ATUAL.CSV"
+MARKET_SHARE_FOLDER = "anp/market_share/extracao/"
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT", "ext-ecole-biomassa")
 BQ_DATASET = "rw_ext_anp"
 TABLE_NAME = "liquidos_vendas_atual"
