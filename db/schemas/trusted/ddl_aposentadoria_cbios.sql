244bfbf9c32feee431ff60f7ef99c53b60719c0a
Luara Raggio <luararaggio@hotmail.com>
2025-09-03 17:56:51 -0300

    feat(feature/106): adds setup for 'aposentadoria cbios' trusted layer

---
+++ db/schemas/trusted/ddl_aposentadoria_cbios.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS td_ext_anp.aposentadoria_cbios (
+-- 	codigo_agente_regulado STRING,
+-- 	cnpj STRING,
+-- 	razao_social STRING,
+-- 	somatorio_emissoes NUMERIC,
+-- 	participacao_mercado NUMERIC,
+-- 	meta_individual_2019 NUMERIC,
+-- 	meta_individual_2019_diaria NUMERIC,
+-- 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+-- ) PARTITION BY DATE(data_criacao);
