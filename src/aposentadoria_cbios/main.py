7067beffe10331916fc81a1bfe36ac8d1861cff1
fenol64 <fenol64.dev@gmail.com>
2025-09-04 14:37:25 -0300

    v1

---
+++ src/aposentadoria_cbios/main.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+import traceback
+import pandas as pd
+import os
+import time
+
+
+def await_file_download(download_dir, timeout=120):
+    start_time = time.time()
+    downloaded_file = None
+
+    while time.time() - start_time < timeout:
+        files = os.listdir(download_dir)
+        for file in files:
+            if file.endswith(".csv"):
+                downloaded_file = os.path.join(download_dir, file)
+                break
+        if downloaded_file:
+            return downloaded_file
+        time.sleep(1)
+
+    raise Exception("File download timed out.")
+
+def configure_driver():
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {
+        "download.default_directory": "/tmp",
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True
+    })
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
+
+def scrape_cbio_data():
+    # Setup Chrome WebDriver
+    driver = configure_driver()
+    try:
+        url = "https://sistemaswebb3-balcao.b3.com.br/historicalSeriesPage/?language=pt-BR"
+        driver.get(url)
+        ELEMENT_ID = "div_idInformation"
+
+        sleep(5)  # Additional wait to ensure all elements are loaded
+        element = driver.find_element(By.ID, ELEMENT_ID)
+        element.click()
+        options_element_a = driver.find_element(By.ID, "4")
+        options_element_a.click()
+        button_element = driver.find_element(By.CSS_SELECTOR, '[b3button]')
+        button_element.click()
+        RADIO_CSS_SELECTOR = 'input[type="radio"][value="yearly"]'
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, RADIO_CSS_SELECTOR)))
+        driver.find_element(By.CSS_SELECTOR, RADIO_CSS_SELECTOR).click()
+        select_year = driver.find_element(By.ID, "year")
+        select_year.click()
+        parent_element = select_year.find_element(By.XPATH, "../..")
+        year_element = parent_element.find_element(By.XPATH, "//a[@role='option' and contains(text(), '2023')]")
+        year_element.click()
+        driver.find_elements(By.CSS_SELECTOR, '[b3button][icon="download"]')[1].click()
+
+        downloaded_file = await_file_download("/tmp", timeout=120)
+
+        if not downloaded_file:
+            raise Exception("File download timed out.")
+
+        print(f"File downloaded to: {downloaded_file}")
+
+        df = pd.read_csv(downloaded_file, sep=';', encoding='latin1')
+        print(df.head())
+
+        if os.path.exists(downloaded_file):
+            os.remove(downloaded_file)
+            print(f"File {downloaded_file} deleted.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        traceback.print_exc()
+
+    finally:
+        # Close the browser
+        # driver.quit()
+        print('Finished')
+
+if __name__ == "__main__":
+    scrape_cbio_data()
