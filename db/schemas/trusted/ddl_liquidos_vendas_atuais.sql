d6a7038fda08b4dbf9d2d02fe91e4b188085b870
Luara Raggio <luararaggio@hotmail.com>
2025-09-04 15:10:38 -0300

    feat(feature/107): adds 'liquidos - vendas atual' trusted schema to bq

--- db/schemas/trusted/ddl_liquidos_vendas_atuais.sql
+++ db/schemas/trusted/ddl_liquidos_vendas_atuais.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS rw_ext_anp.liquidos_vendas_atual (
+CREATE TABLE IF NOT EXISTS td_ext_anp.liquidos_vendas_atual (
     data DATE,
     agente_regulado STRING,
     codigo_produto STRING,
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS rw_ext_anp.liquidos_vendas_atual (
     regiao_destinatario STRING,
     uf_destino STRING,
     mercado_destinatario STRING,
-    quantidade_produto NUMERIC,
+    quantidade_produto_mil_m3 NUMERIC,
 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 ) PARTITION BY DATE(data_criacao);
