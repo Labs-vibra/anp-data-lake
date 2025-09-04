4473500299805ecabe4fdb260abb0e5222b2ae38
Luara Raggio <luararaggio@hotmail.com>
2025-09-04 13:46:49 -0300

    feat(feat/107): adds 'liquidos vendas atuais' trusted schema

---
+++ db/schemas/trusted/ddl_liquidos_vendas_atuais.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS rw_ext_anp.liquidos_vendas_atual (
+    data DATE,
+    agente_regulado STRING,
+    codigo_produto STRING,
+    nome_produto STRING,
+    descricao_produto STRING,
+    regiao_origem STRING,
+    uf_origem STRING,
+    regiao_destinatario STRING,
+    uf_destino STRING,
+    mercado_destinatario STRING,
+    quantidade_produto NUMERIC,
+	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+) PARTITION BY DATE(data_criacao);
