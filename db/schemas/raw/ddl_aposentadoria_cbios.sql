64b25e187624d50da1d78b41ec6f82cb6c7fc48e
fenol64 <fenol64.dev@gmail.com>
2025-09-08 13:44:17 -0300

    description

--- db/schemas/raw/ddl_aposentadoria_cbios.sql
+++ db/schemas/raw/ddl_aposentadoria_cbios.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS rw_ext_anp.aposentadoria_cbios (
-    data STRING,
-    quantidade_parte_obrigada STRING,
-    quantidade_parte_nao_obrigada STRING,
-    totalizacao STRING,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+    data STRING OPTIONS(description="Data de referência da aposentadoria"),
+    quantidade_parte_obrigada STRING OPTIONS(description="Quantidade de CBIOs da parte obrigatória"),
+    quantidade_parte_nao_obrigada STRING OPTIONS(description="Quantidade de CBIOs da parte não obrigatória"),
+    totalizacao STRING OPTIONS(description="Totalização dos CBIOs"),
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de criação do registro")
 ) PARTITION BY DATE(data_criacao);
--- src/aposentadoria_cbios/main.py
+++ src/aposentadoria_cbios/main.py
@@ -82,8 +82,9 @@ def scrape_cbio_data(year = "2023"):
         driver.quit()
         print('Finished')
 
-def cbios_retirement_extract():
-    year_to_extract = os.getenv("YEAR_TO_EXTRACT", "2023")
+def cbios_retirement_extract(date_start=None, date_end=None):
+    if year_to_extract is None:
+        year_to_extract = os.getenv("YEAR_TO_EXTRACT", "2023")
     downloaded_file = scrape_cbio_data(year_to_extract)
     if downloaded_file:
         print(f"File downloaded to: {downloaded_file}")
