2dcbfdc6b3b47cc3a5510c0a5a1d2adb7be7f5ee
Luara Raggio <luararaggio@hotmail.com>
2025-09-09 13:52:17 -0300

    feat(feat/106): adds aposentadoria cbios trusted schema to repo and bq

--- db/schemas/trusted/ddl_aposentadoria_cbios.sql
+++ db/schemas/trusted/ddl_aposentadoria_cbios.sql
@@ -1,10 +1,8 @@
 CREATE TABLE IF NOT EXISTS td_ext_anp.aposentadoria_cbios (
--- 	codigo_agente_regulado STRING,
--- 	cnpj STRING,
--- 	razao_social STRING,
--- 	somatorio_emissoes NUMERIC,
--- 	participacao_mercado NUMERIC,
--- 	meta_individual_2019 NUMERIC,
--- 	meta_individual_2019_diaria NUMERIC,
--- 	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
--- ) PARTITION BY DATE(data_criacao);
+	`data` DATE OPTIONS(description="Data de referência da aposentadoria"),
+    quantidade_parte_obrigada NUMERIC OPTIONS(description="Quantidade de CBIOs da parte obrigatória"),
+    quantidade_parte_nao_obrigada NUMERIC OPTIONS(description="Quantidade de CBIOs da parte não obrigatória"),
+    totalizacao NUMERIC OPTIONS(description="Totalização dos CBIOs"),
+    data_criacao TIMESTAMP OPTIONS(description="Data de criação do registro na camada raw"),
+    data_ingestao_td TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de ingestão na camada trusted")
+) PARTITION BY DATE (data_ingestao_td);
