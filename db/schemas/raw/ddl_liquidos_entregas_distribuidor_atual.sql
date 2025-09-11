413517a9076a53d9b7dcd86dac37d02168da71c8
leeticiamm <leeticiam@hotmail.com>
2025-09-11 13:51:39 -0300

    refatoração da distribuidor anual e fix dml de importação distribuidores

--- db/schemas/raw/ddl_liquidos_entregas_distribuidor_atual.sql
+++ db/schemas/raw/ddl_liquidos_entregas_distribuidor_atual.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS rw_ext_anp.liquidos_entregas_distribuidor_atual (
-    ano STRING,
-    mes STRING,
-    distribuidor STRING,
-    codigo_produto STRING,
-    nome_produto STRING,
-    regiao STRING,
-    quantidade_produto_mil_m3 STRING,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+    ano STRING OPTIONS(description="Ano da entrega"),
+    mes STRING OPTIONS(description="Mês da entrega"),
+    distribuidor STRING OPTIONS(description="Nome do distribuidor"),
+    codigo_produto STRING OPTIONS(description="Código do produto"),
+    nome_produto STRING OPTIONS(description="Nome do produto"),
+    regiao STRING OPTIONS(description="Sigla da região do Brasil"),
+    quantidade_produto_mil_m3 STRING OPTIONS(description="Quantidade do produto em mil m³"),
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de criação do registro")
 ) PARTITION BY DATE(data_criacao);
--- db/schemas/trusted/ddl_market_share_distribuidor_atual.sql
+++ db/schemas/trusted/ddl_market_share_distribuidor_atual.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS td_ext_anp.liquidos_entregas_distribuidor_atual (
-    data DATE,
-    distribuidor STRING,
-    codigo_produto STRING,
-    nome_produto STRING,
-    regiao STRING,
-    quantidade_produto_mil_m3 NUMERIC,
-    data_criacao TIMESTAMP
-) PARTITION BY DATE(data_criacao);
+    id STRING OPTIONS(description="ID único do registro"),
+    data DATE OPTIONS(description="Data da entrega"),
+    distribuidor STRING OPTIONS(description="Nome do distribuidor"),
+    codigo_produto STRING OPTIONS(description="Código do produto"),
+    nome_produto STRING OPTIONS(description="Nome do produto"),
+    regiao STRING OPTIONS(description="Sigla da região do Brasil"),
+    quantidade_produto_mil_m3 NUMERIC OPTIONS(description="Quantidade do produto em mil m³"),
+    data_criacao TIMESTAMP OPTIONS(description="Data de criação do registro"),
+    data_ingestao_td TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de ingestão do registro na camada trusted")
+) PARTITION BY DATE(data_ingestao_td);
