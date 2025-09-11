187423ff965dfe349b451e08866660a832189dcb
dwbessa <danielbessa01@gmail.com>
2025-09-11 18:10:50 -0300

    feat: now relationing codigos Instalacao table with the others to include cnpj and municipio

--- db/queries/refined/dml_rf_logistica.sql
+++ db/queries/refined/dml_rf_logistica.sql
@@ -92,125 +92,143 @@ juncao_vendas_log03 AS (
 ),
 
 sigla_estado_para_nome AS (
-  SELECT
-    *,
+    SELECT
+        *,
     CASE LOWER(sigla_estado_origem)
-      WHEN 'ac' THEN 'acre'
-      WHEN 'al' THEN 'alagoas'
-      WHEN 'ap' THEN 'amapa'
-      WHEN 'am' THEN 'amazonas'
-      WHEN 'ba' THEN 'bahia'
-      WHEN 'ce' THEN 'ceara'
-      WHEN 'df' THEN 'distrito federal'
-      WHEN 'es' THEN 'espirito santo'
-      WHEN 'go' THEN 'goias'
-      WHEN 'ma' THEN 'maranhao'
-      WHEN 'mt' THEN 'mato grosso'
-      WHEN 'ms' THEN 'mato grosso do sul'
-      WHEN 'mg' THEN 'minas gerais'
-      WHEN 'pa' THEN 'para'
-      WHEN 'pb' THEN 'paraiba'
-      WHEN 'pr' THEN 'parana'
-      WHEN 'pe' THEN 'pernambuco'
-      WHEN 'pi' THEN 'piaui'
-      WHEN 'rj' THEN 'rio de janeiro'
-      WHEN 'rn' THEN 'rio grande do norte'
-      WHEN 'rs' THEN 'rio grande do sul'
-      WHEN 'ro' THEN 'rondonia'
-      WHEN 'rr' THEN 'roraima'
-      WHEN 'sc' THEN 'santa catarina'
-      WHEN 'sp' THEN 'sao paulo'
-      WHEN 'se' THEN 'sergipe'
-      WHEN 'to' THEN 'tocantins'
-      ELSE NULL
+        WHEN 'ac' THEN 'acre'
+        WHEN 'al' THEN 'alagoas'
+        WHEN 'ap' THEN 'amapa'
+        WHEN 'am' THEN 'amazonas'
+        WHEN 'ba' THEN 'bahia'
+        WHEN 'ce' THEN 'ceara'
+        WHEN 'df' THEN 'distrito federal'
+        WHEN 'es' THEN 'espirito santo'
+        WHEN 'go' THEN 'goias'
+        WHEN 'ma' THEN 'maranhao'
+        WHEN 'mt' THEN 'mato grosso'
+        WHEN 'ms' THEN 'mato grosso do sul'
+        WHEN 'mg' THEN 'minas gerais'
+        WHEN 'pa' THEN 'para'
+        WHEN 'pb' THEN 'paraiba'
+        WHEN 'pr' THEN 'parana'
+        WHEN 'pe' THEN 'pernambuco'
+        WHEN 'pi' THEN 'piaui'
+        WHEN 'rj' THEN 'rio de janeiro'
+        WHEN 'rn' THEN 'rio grande do norte'
+        WHEN 'rs' THEN 'rio grande do sul'
+        WHEN 'ro' THEN 'rondonia'
+        WHEN 'rr' THEN 'roraima'
+        WHEN 'sc' THEN 'santa catarina'
+        WHEN 'sp' THEN 'sao paulo'
+        WHEN 'se' THEN 'sergipe'
+        WHEN 'to' THEN 'tocantins'
+        ELSE NULL
     END AS nom_estado_origem,
     CASE LOWER(sigla_estado_destino)
-      WHEN 'ac' THEN 'acre'
-      WHEN 'al' THEN 'alagoas'
-      WHEN 'ap' THEN 'amapa'
-      WHEN 'am' THEN 'amazonas'
-      WHEN 'ba' THEN 'bahia'
-      WHEN 'ce' THEN 'ceara'
-      WHEN 'df' THEN 'distrito federal'
-      WHEN 'es' THEN 'espirito santo'
-      WHEN 'go' THEN 'goias'
-      WHEN 'ma' THEN 'maranhao'
-      WHEN 'mt' THEN 'mato grosso'
-      WHEN 'ms' THEN 'mato grosso do sul'
-      WHEN 'mg' THEN 'minas gerais'
-      WHEN 'pa' THEN 'para'
-      WHEN 'pb' THEN 'paraiba'
-      WHEN 'pr' THEN 'parana'
-      WHEN 'pe' THEN 'pernambuco'
-      WHEN 'pi' THEN 'piaui'
-      WHEN 'rj' THEN 'rio de janeiro'
-      WHEN 'rn' THEN 'rio grande do norte'
-      WHEN 'rs' THEN 'rio grande do sul'
-      WHEN 'ro' THEN 'rondonia'
-      WHEN 'rr' THEN 'roraima'
-      WHEN 'sc' THEN 'santa catarina'
-      WHEN 'sp' THEN 'sao paulo'
-      WHEN 'se' THEN 'sergipe'
-      WHEN 'to' THEN 'tocantins'
-      ELSE NULL
+        WHEN 'ac' THEN 'acre'
+        WHEN 'al' THEN 'alagoas'
+        WHEN 'ap' THEN 'amapa'
+        WHEN 'am' THEN 'amazonas'
+        WHEN 'ba' THEN 'bahia'
+        WHEN 'ce' THEN 'ceara'
+        WHEN 'df' THEN 'distrito federal'
+        WHEN 'es' THEN 'espirito santo'
+        WHEN 'go' THEN 'goias'
+        WHEN 'ma' THEN 'maranhao'
+        WHEN 'mt' THEN 'mato grosso'
+        WHEN 'ms' THEN 'mato grosso do sul'
+        WHEN 'mg' THEN 'minas gerais'
+        WHEN 'pa' THEN 'para'
+        WHEN 'pb' THEN 'paraiba'
+        WHEN 'pr' THEN 'parana'
+        WHEN 'pe' THEN 'pernambuco'
+        WHEN 'pi' THEN 'piaui'
+        WHEN 'rj' THEN 'rio de janeiro'
+        WHEN 'rn' THEN 'rio grande do norte'
+        WHEN 'rs' THEN 'rio grande do sul'
+        WHEN 'ro' THEN 'rondonia'
+        WHEN 'rr' THEN 'roraima'
+        WHEN 'sc' THEN 'santa catarina'
+        WHEN 'sp' THEN 'sao paulo'
+        WHEN 'se' THEN 'sergipe'
+        WHEN 'to' THEN 'tocantins'
+        ELSE NULL
     END AS nom_estado_destino
-  FROM juncao_vendas_log03
+    FROM juncao_vendas_log03
 ),
 
 nome_estado_para_regiao AS (
-  SELECT
-    *,
+    SELECT
+        *,
     CASE
-      WHEN nom_estado_origem IN ('acre', 'amapa', 'amazonas', 'para', 'rondonia', 'roraima', 'tocantins') THEN 'norte'
-      WHEN nom_estado_origem IN ('alagoas', 'bahia', 'ceara', 'maranhao', 'paraiba', 'pernambuco', 'piaui', 'rio grande do norte', 'sergipe') THEN 'nordeste'
-      WHEN nom_estado_origem IN ('distrito federal', 'goias', 'mato grosso', 'mato grosso do sul') THEN 'centro-oeste'
-      WHEN nom_estado_origem IN ('espirito santo', 'minas gerais', 'rio de janeiro', 'sao paulo') THEN 'sudeste'
-      WHEN nom_estado_origem IN ('parana', 'rio grande do sul', 'santa catarina') THEN 'sul'
-      ELSE NULL
+        WHEN nom_estado_origem IN ('acre', 'amapa', 'amazonas', 'para', 'rondonia', 'roraima', 'tocantins') THEN 'norte'
+        WHEN nom_estado_origem IN ('alagoas', 'bahia', 'ceara', 'maranhao', 'paraiba', 'pernambuco', 'piaui', 'rio grande do norte', 'sergipe') THEN 'nordeste'
+        WHEN nom_estado_origem IN ('distrito federal', 'goias', 'mato grosso', 'mato grosso do sul') THEN 'centro-oeste'
+        WHEN nom_estado_origem IN ('espirito santo', 'minas gerais', 'rio de janeiro', 'sao paulo') THEN 'sudeste'
+        WHEN nom_estado_origem IN ('parana', 'rio grande do sul', 'santa catarina') THEN 'sul'
+        ELSE NULL
     END AS nom_regiao_origem,
     CASE
-      WHEN nom_estado_destino IN ('acre', 'amapa', 'amazonas', 'para', 'rondonia', 'roraima', 'tocantins') THEN 'norte'
-      WHEN nom_estado_destino IN ('alagoas', 'bahia', 'ceara', 'maranhao', 'paraiba', 'pernambuco', 'piaui', 'rio grande do norte', 'sergipe') THEN 'nordeste'
-      WHEN nom_estado_destino IN ('distrito federal', 'goias', 'mato grosso', 'mato grosso do sul') THEN 'centro-oeste'
-      WHEN nom_estado_destino IN ('espirito santo', 'minas gerais', 'rio de janeiro', 'sao paulo') THEN 'sudeste'
-      WHEN nom_estado_destino IN ('parana', 'rio grande do sul', 'santa catarina') THEN 'sul'
-      ELSE NULL
+        WHEN nom_estado_destino IN ('acre', 'amapa', 'amazonas', 'para', 'rondonia', 'roraima', 'tocantins') THEN 'norte'
+        WHEN nom_estado_destino IN ('alagoas', 'bahia', 'ceara', 'maranhao', 'paraiba', 'pernambuco', 'piaui', 'rio grande do norte', 'sergipe') THEN 'nordeste'
+        WHEN nom_estado_destino IN ('distrito federal', 'goias', 'mato grosso', 'mato grosso do sul') THEN 'centro-oeste'
+        WHEN nom_estado_destino IN ('espirito santo', 'minas gerais', 'rio de janeiro', 'sao paulo') THEN 'sudeste'
+        WHEN nom_estado_destino IN ('parana', 'rio grande do sul', 'santa catarina') THEN 'sul'
+        ELSE NULL
     END AS nom_regiao_destino
   FROM sigla_estado_para_nome
 ),
 
-final_juncao_log03_vendas_atual AS (
-  SELECT
-    razao_social_origem,
-    cod_produto,
-    classe,
-    dsc_produto,
-    sigla_estado_origem,
-    sigla_estado_destino,
-    qtd_produto,
-    nom_estado_origem,
-    nom_estado_destino,
-    nom_regiao_origem,
-    nom_regiao_destino,
+nome_regiao_para_sigla AS (
+    SELECT
+        data,
+        LOWER(REGEXP_REPLACE(NORMALIZE(razao_social_origem, NFD), r'\pM', '')) AS razao_social_origem,
+        LOWER(REGEXP_REPLACE(NORMALIZE(razao_social_destino, NFD), r'\pM', '')) AS razao_social_destino,
+        cod_produto,
+        classe,
+        dsc_produto,
+        sigla_estado_origem,
+        sigla_estado_destino,
+        qtd_produto,
+        nom_estado_origem,
+        nom_estado_destino,
+        nom_regiao_origem,
+        nom_regiao_destino,
     CASE LOWER(nom_regiao_origem)
-      WHEN 'norte' THEN 'N'
-      WHEN 'nordeste' THEN 'NE'
-      WHEN 'centro-oeste' THEN 'CO'
-      WHEN 'sudeste' THEN 'SE'
-      WHEN 'sul' THEN 'S'
-      ELSE NULL
+        WHEN 'norte' THEN 'N'
+        WHEN 'nordeste' THEN 'NE'
+        WHEN 'centro-oeste' THEN 'CO'
+        WHEN 'sudeste' THEN 'SE'
+        WHEN 'sul' THEN 'S'
+        ELSE NULL
     END AS sigla_regiao_origem,
     CASE LOWER(nom_regiao_destino)
-      WHEN 'norte' THEN 'N'
-      WHEN 'nordeste' THEN 'NE'
-      WHEN 'centro-oeste' THEN 'CO'
-      WHEN 'sudeste' THEN 'SE'
-      WHEN 'sul' THEN 'S'
-      ELSE NULL
+        WHEN 'norte' THEN 'N'
+        WHEN 'nordeste' THEN 'NE'
+        WHEN 'centro-oeste' THEN 'CO'
+        WHEN 'sudeste' THEN 'SE'
+        WHEN 'sul' THEN 'S'
+        ELSE NULL
     END AS sigla_regiao_destino
   FROM nome_estado_para_regiao
+),
+
+final_juncao_enriquecida AS (
+    SELECT
+        f.*,
+        i_origem.cnpj_instalacao AS cnpj_instalacao_origem,
+        i_origem.nom_municipio AS nom_municipio_origem,
+        i_destino.cnpj_instalacao AS cnpj_instalacao_destino,
+        i_destino.nom_municipio AS nom_municipio_destino
+    FROM nome_regiao_para_sigla f
+    LEFT JOIN instalacoes i_origem
+        ON LOWER(f.razao_social_origem) = LOWER(i_origem.razao_social)
+        AND LOWER(f.nom_estado_origem) = LOWER(i_origem.nom_estado)
+    LEFT JOIN instalacoes i_destino
+        ON LOWER(f.razao_social_destino) = LOWER(i_destino.razao_social)
+        AND LOWER(f.nom_estado_destino) = LOWER(i_destino.nom_estado)
 )
 
 -- A Query até aqui está incompleta, esse select serve para ver como está o resultado parcial.
 -- Remover as duas primeiras linhas para rodar corretamente.
-SELECT * FROM final_juncao_log03_vendas_atual
+SELECT * FROM final_juncao_enriquecida
