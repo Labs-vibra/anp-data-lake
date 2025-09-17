587151d19d8106912db18adb15cd242afc0a7da2
Luara Raggio <luararaggio@hotmail.com>
2025-09-17 15:01:56 -0300

    refactor(feat/115): change extraction task to raw task in pmqc DAG

--- airflow/dags/pmqc.py
+++ airflow/dags/pmqc.py
@@ -19,18 +19,14 @@ with DAG(
 ) as dag:
 
     with TaskGroup("etl_pmqc", tooltip="ETL PMQC") as etl_pmqc:
-        run_extracao_pmqc = exec_cloud_run_job(
-            task_id="pmqc_extraction",
-            job_name="cr-juridico-extracao-pmqc-job-dev"
+        run_rw_pmqc = exec_cloud_run_job(
+            task_id="pmqc_raw",
+            job_name="cr-juridico-rw-pmqc-job-dev"
         )
-        # run_rw_pmqc = exec_cloud_run_job(
-        # task_id="pmqc_raw",
-        # job_name="cr-juridico-rw-pmqc-job-dev"
-        # )
         # pop_td_pmqc = populate_table(
         #     table="td_ext_anp.pmqc",
         #     sql_name="/sql/trusted/dml_td_pmqc.sql"
         # )
-        run_extracao_pmqc #>> run_rw_pmqc #>>pop_td_pmqc
+        run_rw_pmqc #>>pop_td_pmqc
 
     etl_pmqc
