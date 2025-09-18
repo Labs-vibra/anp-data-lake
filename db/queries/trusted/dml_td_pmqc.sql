90e38eb2e428e9894daecaed11b461dae16a36cd
dwbessa <danielbessa01@gmail.com>
2025-09-18 18:04:30 -0300

    fix: update set fields now corrected, some other fields fixed too, description in schema

--- db/queries/trusted/dml_td_pmqc.sql
+++ db/queries/trusted/dml_td_pmqc.sql
@@ -1,7 +1,7 @@
 MERGE td_ext_anp.pmqc AS target
 USING (
     SELECT
-        FARM_FINGERPRINT(CONCAT(id_numeric, '-', data_coleta, '-', cnpj_posto, '-', distribuidora, '-', grupo_produto, '-', produto)) AS id,
+        FARM_FINGERPRINT(CONCAT(id_numeric, '-', data_coleta, '-', cnpj_posto, '-', distribuidora, '-', grupo_produto, '-', produto, '-', ensaio, '-', longitude, '-', latitude)) AS id,
         PARSE_DATE('%Y-%m-%d', data_coleta) AS data_coleta,
         id_numeric,
         REGEXP_REPLACE(LOWER(TRIM(NORMALIZE(grupo_produto, NFD))), '[^a-zA-Z0-9_\\s-.\']', '') AS grupo_produto,
@@ -21,7 +21,6 @@ USING (
         REGEXP_REPLACE(LOWER(TRIM(NORMALIZE(resultado, NFD))), '[^a-zA-Z0-9_\\s-.\']', '') AS resultado,
         REGEXP_REPLACE(LOWER(TRIM(NORMALIZE(unidade_ensaio, NFD))), '[^a-zA-Z0-9_\\s-.\']', '') AS unidade_ensaio,
         REGEXP_REPLACE(LOWER(TRIM(NORMALIZE(conforme, NFD))), '[^a-zA-Z0-9_\\s-.\']', '') AS conforme,
-        CURRENT_TIMESTAMP() AS data_ingestao_td,
         data_criacao
     FROM
         rw_ext_anp.pmqc
@@ -33,26 +32,9 @@ USING (
 ON target.id = source.id
 WHEN MATCHED THEN
     UPDATE SET
-        data_coleta = source.data_coleta,
-        id_numeric = source.id_numeric,
-        grupo_produto = source.grupo_produto,
-        produto = source.produto,
-        razao_social_posto = source.razao_social_posto,
-        cnpj_posto = source.cnpj_posto,
-        distribuidora = source.distribuidora,
-        endereco = source.endereco,
-        complemento = source.complemento,
-        bairro = source.bairro,
-        municipio = source.municipio,
-        latitude = source.latitude,
-        longitude = source.longitude,
-        uf = source.uf,
-        regiao_politica = source.regiao_politica,
-        ensaio = source.ensaio,
         resultado = source.resultado,
         unidade_ensaio = source.unidade_ensaio,
         conforme = source.conforme,
-        data_criacao = source.data_criacao
 WHEN NOT MATCHED THEN
     INSERT (
         id,
@@ -75,8 +57,7 @@ WHEN NOT MATCHED THEN
         resultado,
         unidade_ensaio,
         conforme,
-        data_criacao,
-        data_ingestao_td
+        data_criacao
     )
     VALUES (
         source.id,
@@ -99,6 +80,5 @@ WHEN NOT MATCHED THEN
         source.resultado,
         source.unidade_ensaio,
         source.conforme,
-        source.data_criacao,
-        source.data_ingestao_td
+        source.data_criacao
     )
