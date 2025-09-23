923984acc2dece7851de031e9033860182e190a7
Luara Raggio <luararaggio@hotmail.com>
2025-09-23 15:55:08 -0300

    fix(feat/126): changes job description in 'tancagem do abastecimento nacional de combustiveis' terraform configs

--- terraform/modules/pmqc/main.tf
+++ terraform/modules/pmqc/main.tf
@@ -1,11 +1,11 @@
 resource "google_cloud_run_v2_job" "raw_pmqc_job" {
-  name     = "cr-juridico-rw-tancagem-do-abastecimento-nacional-de-combustiveis-job-dev"
+  name     = "cr-juridico-rw-pmqc-job-dev"
   location = var.region
 
   template {
     template {
       containers {
-        image = "${var.jobs_image_base_url}/run-raw-tancagem-do-abastecimento-nacional-de-combustiveis-job:${var.image_version}"
+        image = "${var.jobs_image_base_url}/run-raw-pmqc-job:${var.image_version}"
       }
     }
   }
