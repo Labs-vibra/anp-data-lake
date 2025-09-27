b8fe863529b50f9b84f78a73c6549926ff017dc9
7vanger <vitor.lgdasilva@gmail.com>
2025-09-27 13:00:35 -0300

    Criação da extração e raw do arquivo produção de biodiesel m3 geral

--- src/producao_biodiesel_m3/producao_biodiesel_m3_geral.py
+++ src/producao_biodiesel_m3/producao_biodiesel_m3_geral.py
@@ -4,13 +4,15 @@ import pandas as pd
 from datetime import date
 from google.cloud import storage, bigquery
 import requests
+from bs4 import BeautifulSoup
+from utils import get_latest_links, normalize_column
 from constants import (
 #    BUCKET_NAME,
 #    MARKET_SHARE_FOLDER,
-#    PROJECT_ID,
-#    BQ_DATASET,
-#    TABLE_NAME,
-    MAPPING_COLUMNS
+    PROJECT_ID,
+    BQ_DATASET,
+    TABLE_NAME_GERAL,
+    COLUMNS_GERAL
 )
 import logging
 
@@ -23,7 +25,7 @@ def insert_data_into_bigquery(df: pd.DataFrame) -> None:
     """ Insere dados no BigQuery com particionamento por data. """
 
     bq_client = bigquery.Client()
-    table_id = f"{PROJECT_ID}.{BQ_DATASET}.{TABLE_NAME}"
+    table_id = f"{PROJECT_ID}.{BQ_DATASET}.{TABLE_NAME_GERAL}"
 
     job_config = bigquery.LoadJobConfig(
          write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
@@ -36,7 +38,7 @@ def insert_data_into_bigquery(df: pd.DataFrame) -> None:
     )
     job.result()
 
-def rw_ext_anp_producao_biodiesel_barris():
+def rw_producao_biodiesel_geral(): 
     """
     Faz download do arquivo Liquidos_Vendas_Atuais.csv mais recente do bucket no GCP,
     lê o arquivo, formata colunas e sobe a camada raw para o BigQuery.
@@ -46,29 +48,39 @@ def rw_ext_anp_producao_biodiesel_barris():
     #bucket = storage_client.bucket(BUCKET_NAME)
     
     #blobs = list(bucket.list_blobs(prefix=MARKET_SHARE_FOLDER))
-    #lobs_filtrados = [b for b in blobs if "producao_biodiesel_barris" in b.name]
+    #lobs_filtrados = [b for b in blobs if "producao_biodiesel_m3_geral" in b.name]
     #if not blobs_filtrados:
-    #    raise FileNotFoundError("Nenhum arquivo encontrado com 'producao_biodiesel_barris' no nome.")
+    #    raise FileNotFoundError("Nenhum arquivo encontrado com 'producao_biodiesel_m3_geral' no nome.")
     #latest_blob = max(blobs_filtrados, key=lambda b: b.updated)
 
     #logging.info(f"Baixando arquivo {latest_blob.name} do bucket {BUCKET_NAME}...")
     #data_bytes = latest_blob.download_as_bytes()
+    links = get_latest_links()
+    if not links:
+        raise FileNotFoundError("Nenhum CSV encontrado no site da ANP.")
 
-    url_csv = "https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/arquivos-producao-de-biocombustiveis/producao-biodiesel-m3-2005-2022.csv"
+    link = [l for l in links if "2005" in l][-1]
+
+    print(f"Baixando geral: {link}")
 
     # --- CSV ---
-    response_csv = requests.get(url_csv)
+    response_csv = requests.get(link)
     response_csv.raise_for_status()
 
-    df = pd.read_csv(BytesIO(response_csv.content), sep=";", encoding="latin1")
-    df.to_csv("producao_biodiesel_m3.csv", index=False, sep=";", encoding="utf-8")
+    df = pd.read_csv(BytesIO(response_csv.content), sep=";", encoding="utf-8")
+    logging.info(f"Arquivo carregado com {len(df)} registros.")
+    print("Arquivo salvo com sucesso!")
 
-    print("Arquivos salvos com sucesso!")
-    df.rename(columns=MAPPING_COLUMNS, inplace=True)
-
-    #insert_data_into_bigquery(df)
-    #logging.info("Inserção de dados concluída.")
-
+    try:
+        logging.info(f"Arquivo carregado com {len(df)} registros.")
+        df.columns = [normalize_column(c) for c in df.columns]
+        #insert_data_into_bigquery(df)
+        #logging.info("Inserção de dados concluída.")
+    except Exception as e:
+        logger.warning(f"Erro ao processar {link}: {e}")
+    
+    return df
 
 if __name__ == "__main__":
-    rw_ext_anp_producao_biodiesel_barris()
+    df = rw_producao_biodiesel_geral()
+    print(df.head())
