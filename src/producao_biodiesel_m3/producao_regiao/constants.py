87efc2f43aba07572f88690c9e10dd2676873747
7vanger <vitor.lgdasilva@gmail.com>
2025-09-30 15:15:17 -0300

    Alterações na extração de produção de m3 regiao

--- src/producao_biodiesel_m3/producao_regiao/constants.py
+++ src/producao_biodiesel_m3/producao_regiao/constants.py
@@ -8,25 +8,19 @@ BUCKET_NAME= os.getenv("BUCKET_NAME", "ext-ecole-biomassa")
 #BIO_FOLDER = ""
 PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT", "ext-ecole-biomassa")
 BQ_DATASET = "rw_ext_anp"
-TABLE_NAME_GERAL = "producao_biodiesel_m3_geral"
+TABLE_NAME_REGIAO = "producao_biodiesel_m3_regiao"
 
-MAPPING_COLUMNS_GERAL = {
+MAPPING_COLUMNS_REGIAO = {
     "ANO": "ano",
     "MÊS": "mes",
     "GRANDE REGIÃO": "grande_regiao",
-    "UNIDADE DA FEDERAÇÃO": "unidade_da_federacao",
-    "PRODUTOR": "produtor",
-    "PRODUTO": "produto",
     "PRODUÇÃO": "producao",
 }
 
-COLUMNS_GERAL = {
+COLUMNS_REGIAO = {
     'ano',
     'mes',
     'grande_regiao',
-    'unidade_da_federacao',
-    'produtor',
-    'produto',
     'producao'
 }
 
