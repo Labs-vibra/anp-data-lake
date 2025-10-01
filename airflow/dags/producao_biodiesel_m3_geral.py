306e04df28e2904f055dd473a76b1f3fb44a07a7
7vanger <vitor.lgdasilva@gmail.com>
2025-10-01 16:42:00 -0300

    Criação da dag refined

---
+++ airflow/dags/producao_biodiesel_m3_geral.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
+from utils.operators import exec_job, populate_table
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='producao_biodiesel_m3_geral_pipeline',
+    default_args=default_args,
+    description='Produção de biodiesel m3 geral',
+    schedule_interval='@monthly',
+    catchup=False,
+    max_active_tasks=2,
+) as dag:
+
+    with TaskGroup("etl_producao_biodiesel_m3_geral", tooltip="ETL Produção de biodiesel m3 geral") as etl_producao_biodiesel_m3_geral:
+        pop_rf_producao_biodiesel_m3_geral = populate_table(
+            table="rf_ext_anp.producao_biodiesel_m3_geral",
+            sql_name=f"/sql/refined/dml_rf_producao_biodiesel_m3_geral.sql"
+        )
+
+        pop_rf_producao_biodiesel_m3_geral
