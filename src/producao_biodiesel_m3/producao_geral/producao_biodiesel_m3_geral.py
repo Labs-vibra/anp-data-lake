0707e7195868be93c44f9b4d33f78d4c1cf5775f
7vanger <vitor.lgdasilva@gmail.com>
2025-10-01 14:58:26 -0300

    Alteração na extração no arquivo de extração produção bio m3

--- src/producao_biodiesel_m3/producao_geral/producao_biodiesel_m3_geral.py
+++ src/producao_biodiesel_m3/producao_geral/producao_biodiesel_m3_geral.py
@@ -42,18 +42,6 @@ def rw_producao_biodiesel_geral():
     Faz download do arquivo produção de biodiesel m3 geral do site anp,
     lê o arquivo, formata colunas e sobe a camada raw para o BigQuery.
     """
-    #storage_client = storage.Client()
-
-    #bucket = storage_client.bucket(BUCKET_NAME)
-    
-    #blobs = list(bucket.list_blobs(prefix=MARKET_SHARE_FOLDER))
-    #lobs_filtrados = [b for b in blobs if "producao_biodiesel_m3_geral" in b.name]
-    #if not blobs_filtrados:
-    #    raise FileNotFoundError("Nenhum arquivo encontrado com 'producao_biodiesel_m3_geral' no nome.")
-    #latest_blob = max(blobs_filtrados, key=lambda b: b.updated)
-
-    #logging.info(f"Baixando arquivo {latest_blob.name} do bucket {BUCKET_NAME}...")
-    #data_bytes = latest_blob.download_as_bytes()
     links = get_latest_links()
     if not links:
         raise FileNotFoundError("Nenhum CSV encontrado no site da ANP.")
@@ -73,8 +61,8 @@ def rw_producao_biodiesel_geral():
     try:
         logging.info(f"Arquivo carregado com {len(df)} registros.")
         df.columns = [normalize_column(c) for c in df.columns]
-        #insert_data_into_bigquery(df)
-        #logging.info("Inserção de dados concluída.")
+        insert_data_into_bigquery(df)
+        logging.info("Inserção de dados concluída.")
     except Exception as e:
         logger.warning(f"Erro ao processar {link}: {e}")
     
