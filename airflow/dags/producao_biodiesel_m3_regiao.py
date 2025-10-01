601f9e4f023001a79b46480c06a0ec3bb886a1b6
7vanger <vitor.lgdasilva@gmail.com>
2025-10-01 16:01:08 -0300

    Alteração no arquivo dag de produção de regiao

--- airflow/dags/producao_biodiesel_m3_regiao.py
+++ airflow/dags/producao_biodiesel_m3_regiao.py
@@ -34,7 +34,3 @@ with DAG(
 
         run_rw_producao_biodiesel_m3_regiao >> pop_td_producao_biodiesel_m3_regiao >> pop_rf_producao_biodiesel_m3_regiao
 
-<<<<<<< HEAD
-=======
-        run_rw_producao_biodiesel_m3_regiao >> pop_td_producao_biodiesel_m3_regiao
->>>>>>> trusted_producao_m3_regiao
