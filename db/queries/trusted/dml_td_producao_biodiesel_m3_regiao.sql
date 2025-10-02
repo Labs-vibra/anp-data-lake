8434cc6875b0c85f937ba665864c24c0666e6dc5
7vanger <vitor.lgdasilva@gmail.com>
2025-10-02 13:17:19 -0300

    Criação do dml da produção de região

---
+++ db/queries/trusted/dml_td_producao_biodiesel_m3_regiao.sql
@@ -0,0 +1,35 @@
+MERGE td_ext_anp.producao_biodiesel_m3_geral AS target
+USING (
+    SELECT
+        FARM_FINGERPRINT(CONCAT(ano, mes, grande_regiao, unidade_federacao, produtor, produto)) AS id,
+        PARSE_DATE('%Y-%m-%d', CONCAT(ano, '-', mes, '-01')) AS data,
+        LOWER(REGEXP_REPLACE(NORMALIZE(grande_regiao, NFD), r'\pM', '')) AS grande_regiao,
+        IFNULL(SAFE_CAST(REPLACE(producao, ',', '.') AS NUMERIC), 0) AS producao,
+        data_criacao
+    FROM rw_ext_anp.producao_biodiesel_m3_geral
+    WHERE data_criacao = (
+        SELECT MAX(data_criacao)
+        FROM rw_ext_anp.producao_biodiesel_m3_geral
+    )
+) AS source
+ON  target.data                = source.data
+AND target.grande_regiao       = source.grande_regiao
+WHEN MATCHED THEN
+    UPDATE SET
+        producao = source.producao,
+        data_criacao = source.data_criacao
+WHEN NOT MATCHED THEN
+    INSERT (
+        id,
+        data,
+        grande_regiao,
+        producao,
+        data_criacao
+    )
+    VALUES (
+        source.id,
+        source.data,
+        source.grande_regiao,
+        source.producao,
+        source.data_criacao
+    );
