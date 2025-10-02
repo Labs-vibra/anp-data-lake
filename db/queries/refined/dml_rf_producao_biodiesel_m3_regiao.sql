cb2cdb78fab91798cb82a83daeb559fc40ab7eea
7vanger <vitor.lgdasilva@gmail.com>
2025-10-02 13:53:25 -0300

    Criação da refined de produção região

---
+++ db/queries/refined/dml_rf_producao_biodiesel_m3_regiao.sql
@@ -0,0 +1,39 @@
+MERGE rf_ext_anp.ft_anp_bio_producao_regiao AS target
+USING (
+    SELECT
+        id AS anp_chv_ide,
+        data AS anp_dat_producao,
+        grande_regiao AS anp_dsc_grande_regiao,
+        producao AS anp_qtd_producao_m3,
+        producao * 6.28981 AS anp_qtd_producao_barris,
+        data_ingestao_td
+    FROM td_ext_anp.producao_biodiesel_m3_regiao
+    WHERE data_criacao = (
+        SELECT MAX(data_ingestao_td)
+        FROM td_ext_anp.producao_biodiesel_m3_regiao
+    )
+) AS source
+ON  target.anp_dat_producao         = source.anp_dat_producao
+AND target.anp_dsc_grande_regiao    = source.anp_dsc_grande_regiao
+WHEN MATCHED THEN
+    UPDATE SET
+        anp_qtd_producao_m3     = source.anp_qtd_producao_m3,
+        anp_qtd_producao_barris = source.anp_qtd_producao_barris,
+        data_ingestao_td        = source.data_ingestao_td
+WHEN NOT MATCHED THEN
+    INSERT (
+        anp_chv_ide,
+        anp_dat_producao,
+        anp_dsc_grande_regiao,
+        anp_qtd_producao_m3,
+        anp_qtd_producao_barris,
+        data_ingestao_td
+    )
+    VALUES (
+        source.anp_chv_ide,
+        source.anp_dat_producao,
+        source.anp_dsc_grande_regiao,
+        source.anp_qtd_producao_m3,
+        source.anp_qtd_producao_barris,
+        source.data_ingestao_td
+    );
