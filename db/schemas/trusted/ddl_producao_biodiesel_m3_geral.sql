53003382e07f6194075a4c5f7dab3026331a5466
7vanger <vitor.lgdasilva@gmail.com>
2025-10-02 13:25:11 -0300

    alteração do código ddl produção geral

--- db/schemas/trusted/ddl_producao_biodiesel_m3_geral.sql
+++ db/schemas/trusted/ddl_producao_biodiesel_m3_geral.sql
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS td_ext_anp.producao_biodiesel_m3_geral (
     produtor STRING OPTIONS(description="Nome do produtor"),
     produto STRING OPTIONS(description="Nome do produto"),
     producao NUMERIC OPTIONS(description="Quantidade do produto em m³"),
-    data_criacao TIMESTAMP OPTIONS(description="Data de inserção dos dados na camada raw")
+    data_criacao TIMESTAMP OPTIONS(description="Data de inserção dos dados na camada raw"),
     data_ingestao_td TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de inserção dos dados na camada trusted")
 ) PARTITION BY DATE(data_ingestao_td);
