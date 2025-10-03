e87b83776df38188e9459493574d3d6877157e10
dwbessa <danielbessa01@gmail.com>
2025-10-03 09:00:22 -0300

    fix: outputs removidos

--- terraform/modules/postos_revendedores/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "postos_revendedores_job_name" {
-  description = "Nome do job de Postos Revendedores"
-  value       = google_cloud_run_v2_job.extracao_postos_revendedores_job.name
-}
--- terraform/modules/vendas_comb_segmento/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vendas_comb_segmento_job_name" {
-  description = "Nome do job vendas de combustíveis por segmento"
-  value       = google_cloud_run_v2_job.extracao_vendas_comb_segmento_job.name
-}
