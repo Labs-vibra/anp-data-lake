f298bb5ec5655e105987ec23141ef01cce637692
7vanger <vitor.lgdasilva@gmail.com>
2025-10-06 15:19:16 -0300

    Options refined regiao

--- db/schemas/refined/ddl_rf_producao_biodiesel_m3_regiao.sql
+++ db/schemas/refined/ddl_rf_producao_biodiesel_m3_regiao.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS rf_ext_anp.ft_anp_bio_producao_regiao (
     anp_dsc_grande_regiao STRING OPTIONS(description="Grande região do Brasil"),
     anp_qtd_producao_m3 NUMERIC OPTIONS(description="Quantidade produzida em metros cúbicos (m³)"),
     anp_qtd_producao_barris NUMERIC OPTIONS(description="Quantidade produzida convertida para barris"),
-    data_ingestao_td TIMESTAMP (description="Data da inserção dos dados na camada trusted"),
+    data_ingestao_td TIMESTAMP OPTIONS(description="Data da inserção dos dados na camada trusted"),
     data_ingestao_rf TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de inserção dos dados na camada refined")
 )
 PARTITION BY DATE(data_ingestao_rf)
