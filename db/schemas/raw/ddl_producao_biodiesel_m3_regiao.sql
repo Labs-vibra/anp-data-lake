fee073e12fa8d76f51ad842f87e14446e6daedfc
Luara Raggio <luararaggio@hotmail.com>
2025-10-27 17:23:01 -0300

    fix(deploy): add fields to 'producao de biodiesel regiao' schemas

--- db/schemas/raw/ddl_producao_biodiesel_m3_regiao.sql
+++ db/schemas/raw/ddl_producao_biodiesel_m3_regiao.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS rw_ext_anp.producao_biodiesel_m3_regiao (
     ano STRING OPTIONS(description="Ano da producao"),
     mes STRING OPTIONS(description="Mês da producao"),
     grande_regiao STRING OPTIONS(description="Região do Brasil"),
+    unidade_da_federacao STRING OPTIONS(description="Unidade de Federação"),
+    produto STRING OPTIONS(description="Nome do produto"),
     producao STRING OPTIONS(description="Quantidade do produto em m³"),
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de criação do registro")
 ) PARTITION BY DATE(data_criacao);
--- db/schemas/trusted/ddl_producao_biodiesel_m3_regiao.sql
+++ db/schemas/trusted/ddl_producao_biodiesel_m3_regiao.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS td_ext_anp.producao_biodiesel_m3_regiao (
     data DATE OPTIONS(description="Data da producao"),
     grande_regiao STRING OPTIONS(description="Região do Brasil"),
     producao NUMERIC OPTIONS(description="Quantidade do produto em m³"),
+    unidade_da_federacao STRING OPTIONS(description="Unidade de Federação"),
+    produto STRING OPTIONS(description="Nome do produto"),
     data_criacao TIMESTAMP OPTIONS(description="Data de inserção dos dados na camada raw"),
     data_ingestao_td TIMESTAMP DEFAULT CURRENT_TIMESTAMP() OPTIONS(description="Data de inserção dos dados na camada trusted")
 ) PARTITION BY DATE(data_ingestao_td);
